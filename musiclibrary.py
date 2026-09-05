c3cbe99862a3b846929ce2853e7c1cda9807e7d6
dhruvv <drippxdhruv@gmail.com>
2026-09-05 20:12:58 +0530

    Kath Lagda": "https://www.youtube.com/watch?v=nMozMETeCyw",        "Hood Ambience": "https://www.youtube.com/watch?v=qs61wmCGBhs",        "City Dreams": "https://www.youtube.com/watch?v=OUUWLyfn3hY",        "Moodshift": "https://www.youtube.com/watch?v=8cxMyUx-T74",        "She Aint Easy": "https://www.youtube.com/watch?v=p4s7iPXlGlI",        "Kaare": "https://www.youtube.com/watch?v=kr_CAcR0Bz4",         "Headliner": "https://www.youtube.com/watch?v=GE6w7DRyqcM",
            webbrowser.open('https://reddit.com')

--- musiclibrary.py
+++ musiclibrary.py
@@ -3,5 +3,16 @@ music={'the finest': 'https://youtu.be/Ld9hW_ztn_c',
        'dusky': 'https://youtu.be/acnHDzmxwyc',
        'piche tere': 'https://youtu.be/SZisWzyBdHI',
        'kurbaan hua':'https://youtu.be/gCNmhrvplpw',
+       "Kath Lagda": "https://www.youtube.com/watch?v=nMozMETeCyw",
+       "Hood Ambience": "https://www.youtube.com/watch?v=qs61wmCGBhs",
+       "City Dreams": "https://www.youtube.com/watch?v=OUUWLyfn3hY",
+       "Moodshift": "https://www.youtube.com/watch?v=8cxMyUx-T74",
+       "She Aint Easy": "https://www.youtube.com/watch?v=p4s7iPXlGlI",
+       "Star Girl": "https://www.youtube.com/watch?v=RerxGZCLhyA",
+       "Kaare": "https://www.youtube.com/watch?v=kr_CAcR0Bz4",
+       "Headliner": "https://www.youtube.com/watch?v=GE6w7DRyqcM",
        
+
+     
+
        }
